ff9dad5e07b4ee932eaf70d91c3efcf877895b45
Cristian Orozco <cristianorozco1984@gmail.com>
2019-07-08 16:06:27 -0500

    Team Section Ready

--- src/js/script.js
+++ src/js/script.js
@@ -23,6 +23,24 @@ $("#wrapper").click( function() {
 // Parallax effects
 const rellax = new Rellax('.rellax-effect', {
   callback: function(position){
-
   }
 })
+
+
+// Swiper Team Section
+const swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: '.swiper-pagination'
+  },
+});
